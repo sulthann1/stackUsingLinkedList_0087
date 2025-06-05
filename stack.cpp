09d62047630939ce343ed361bef5a419da919180
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-05 17:34:01 +0700

    dengan output push value

--- stack.cpp
+++ stack.cpp
@@ -26,6 +26,12 @@ class Stack
 
     int push(int value)
     {
-        
+        Node *newNode = new Node ();
+        newNode -> data = value;
+        newNode -> next = top;
+        top = newNode;
+        cout << "Push Value :" <<value <<endl;
+        return value;
+
     }
 };
