e573440290a75dbec111e48158a4f77364a2d24d
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-05 18:02:28 +0700

    cout <<endl;

--- stack.cpp
+++ stack.cpp
@@ -106,11 +106,18 @@ int main()
 
             case 4 :
             cout <<"Exiting program." <<endl; 
+            break;
+            default :
+            cout <<"Invalid choice.Try again." <<endl;
+            break;
 
         }
 
+        cout <<endl;
 
     }
+
+    
 
 
     
